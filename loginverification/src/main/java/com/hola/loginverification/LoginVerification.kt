25bddac5e7447dd2bdab186c0113bd919c5fc198
Mwiczkowski <miloszwiczkowski@gmail.com>
2020-03-22 14:09:00 +0100

    Change test password

--- loginverification/src/main/java/com/hola/loginverification/LoginVerification.kt
+++ loginverification/src/main/java/com/hola/loginverification/LoginVerification.kt
@@ -3,7 +3,7 @@ package com.hola.loginverification
 class LoginVerification {
     fun verifyCredentials(email: String, password: String): Boolean {
         return (email == "test@holaola.com" &&
-                password == "test123") ||
+                password == "test1234") ||
                 (email == "admin@holaola.com" &&
                         password == "admin1234")
     }
